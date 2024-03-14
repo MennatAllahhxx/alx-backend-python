2d9f7c04170cffb9132973c6ff1966bcdec13b4a
Menna Hassan <h.menna85@gmail.com>
2024-03-14 23:44:46 +0200

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module contains add fun
+"""
+
+
+def add(a: float, b: float) -> float:
+    """AI is creating summary for add
+
+    Args:
+        a (float): 1st arg
+        b (float): 2nd arg
+
+    Returns:
+        float: sum of a and b
+    """
+    return (a + b)
