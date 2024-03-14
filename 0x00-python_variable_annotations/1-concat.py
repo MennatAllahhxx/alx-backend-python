22a7505ed38435bc64fc12ae9fb800ee4ac24e3c
Menna Hassan <h.menna85@gmail.com>
2024-03-14 23:54:05 +0200

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module contains concat fun
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """AI is creating summary for coccat
+
+    Args:
+        str1 (str): 1st string
+        str2 (str): 2nd string
+
+    Returns:
+        str: concatenated string
+    """
+    return str1+str2
