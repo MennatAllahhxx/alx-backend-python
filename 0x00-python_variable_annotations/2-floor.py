99daa34e61a4083ca0dcfefc1a8875a8287386b8
Menna Hassan <h.menna85@gmail.com>
2024-03-15 00:07:12 +0200

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module contains floor fun
+"""
+
+
+def floor(n: float) -> int:
+    """AI is creating summary for floor
+
+    Args:
+        n (float): [description]
+
+    Returns:
+        int: [description]
+    """
+    return int(n)
