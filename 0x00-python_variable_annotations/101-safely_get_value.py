e6ec57b4fa34d7bbc50b8669455e459dbb1d1f50
Menna Hassan <h.menna85@gmail.com>
2024-03-15 02:46:00 +0200

    task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module contains safe_first_element fun
+"""
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    """AI is creating summary for safely_get_value
+
+    Args:
+        dct (Mapping): a dictionary
+        key (Any): key of the value to be searched for
+        default (Union[T, None], optional): TypeVar. Defaults to None.
+
+    Returns:
+        Union[Any, T]: default or value of dct[key]
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
