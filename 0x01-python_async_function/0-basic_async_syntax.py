3b56723e36ddce8b6f190df2fbe28fbbde8b970d
Menna Hassan <h.menna85@gmail.com>
2024-03-18 16:32:03 +0200

    task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,11 +7,12 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay=10):
     """AI is creating summary for wait_random
 
     Args:
-        max_delay (int, optional): maximum delay for the coroutine. Defaults to 10.
+        max_delay (int, optional): maximum delay for the coroutine.
+        Defaults to 10.
 
     Returns:
         float: the delay of the function resulting from random function
