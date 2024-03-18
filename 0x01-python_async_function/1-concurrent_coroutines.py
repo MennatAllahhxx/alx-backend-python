3311ea640344af8162b2185ff5790aa570e26f38
Menna Hassan <h.menna85@gmail.com>
2024-03-18 16:49:10 +0200

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module contains wait_n coroutine
+"""
+
+import asyncio
+from random import uniform
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """AI is creating summary for wait_n
+
+    Args:
+        n (int): number of times to perform wait_random.
+        max_delay (int): max delay to be sent to wait_random.
+
+    Returns:
+        List[float]: a sorted list of the returning values from wait_random.
+    """
+    lst: List[float] = []
+
+    for i in range(n):
+        lst.append(await wait_random(max_delay))
+
+    lst.sort()
+    return lst
