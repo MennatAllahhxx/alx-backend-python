0493c82b270dd76c3c36019db35a6774c9d01651
Menna Hassan <h.menna85@gmail.com>
2024-03-18 16:27:26 +0200

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module contains wait_random coroutine
+"""
+
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay = 10):
+    """AI is creating summary for wait_random
+
+    Args:
+        max_delay (int, optional): maximum delay for the coroutine. Defaults to 10.
+
+    Returns:
+        float: the delay of the function resulting from random function
+    """
+    random_delay = uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
