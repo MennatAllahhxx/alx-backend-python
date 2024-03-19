a90dbc4b65681112e9da697eacba1b59b1f8a204
Menna Hassan <h.menna85@gmail.com>
2024-03-19 23:04:37 +0200

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module contains async_generator coroutine
+"""
+
+import asyncio
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
+    """AI is creating summary for async_generator
+
+    Returns:
+        float: a random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
