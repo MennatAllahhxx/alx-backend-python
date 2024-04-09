d39d22c1675e73bac0cffe0ae34bfff836b9fb14
Menna Hassan <h.menna85@gmail.com>
2024-04-09 04:27:58 +0200

    task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-Module contains TestAccessNestedMap class
+Module contains TestAccessNestedMap and TestGetJson classes
 """
 import unittest
+import requests
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
-
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,7 +30,6 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-
     @parameterized.expand([
         ({}, "a", KeyError),
         ({"a": 1}, ("a", "b"), KeyError)
@@ -44,6 +44,29 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(expected):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """AI is creating summary for TestGetJson
+
+    Args:
+        unittest (module): a module to create unittests
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """AI is creating summary for test_get_json
+
+        Args:
+            test_url (str): url to get json for
+            test_payload (Dict): payload for that url
+        """
+        with patch('requests.get') as mock_read:
+            mock_read.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_read.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
